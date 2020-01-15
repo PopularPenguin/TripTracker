72478aa0c54fa169a821c5adcde9ec99860e4a2d
Brian Niedzialkoski <brianniedzialkoski@gmail.com>
2020-01-15 17:32:34 -0500

    Slightly improve distance tracker widget looks

--- app/src/main/java/com/popularpenguin/triptracker/common/ViewAnimationUtils.kt
+++ app/src/main/java/com/popularpenguin/triptracker/common/ViewAnimationUtils.kt
@@ -5,37 +5,6 @@ import android.view.animation.TranslateAnimation
 
 object ViewAnimationUtils {
 
-    // Info view displayed in the top-left corner of the screen
-    fun hideInfoView(view: View) {
-        // Slide left and up off the screen
-        val hideAnimation = TranslateAnimation(
-            0.0f,
-            -view.width.toFloat(),
-            view.height.toFloat(),
-            -view.height.toFloat()
-        ).apply {
-            duration = 200
-            fillAfter = true
-        }
-
-        view.startAnimation(hideAnimation)
-    }
-
-    fun showInfoView(view: View) {
-        // Slide right and down into screen
-        val showAnimation = TranslateAnimation(
-            -view.width.toFloat(),
-            0.0f,
-            0.0f,
-            view.height.toFloat()
-        ).apply {
-            duration = 200
-            fillAfter = true
-        }
-
-        view.startAnimation(showAnimation)
-    }
-
     // Photo RecyclerView shown in SingleTripFragment
     fun hidePhotoView(view: View) {
         // Slide down off the screen
